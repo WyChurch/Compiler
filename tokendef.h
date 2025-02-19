ab0eb16fc827ce76f616b57797aa040b7788b2a6
jdr357 <159956934+jdr357@users.noreply.github.com>
2025-02-19 17:55:56 -0600

    Revert "Updated keywords"
    This reverts commit 30ccaa82bdf1b345a6d9d024a4c98abca35e50b7.

--- tokendef.h
+++ tokendef.h
@@ -8,12 +8,28 @@
 #define KWD_IF          255
 #define KWD_ELSE        256
 #define KWD_WHILE       257
-
+#define KWD_DO          258
+#define KWD_BREAK       259
+#define KWD_CONTINUE    260
 #define KWD_INT         261
+#define KWD_DOUBLE      262
+#define KWD_FLOUT       263
 #define KWD_RETURN      264
 #define KWD_CHAR        265
+#define KWD_CASE        266
+#define KWD_SIZEOF      267
+#define KWD_LONG        268
+#define KWD_SHORT       269
+#define KWD_TYPEDEF     270
+#define KWD_SWITCH      271
+#define KWD_UNSIGNED    272
 #define KWD_VOID        273
+#define KWD_STATIC      274
+#define KWD_STRUCK      275
+#define KWD_GOTO        276
 #define KWD_STRING      277
+#define KWD_DEFAULT     278
+#define KWD_SIGNED      279
 
 /* operators */
 #define OPER_ADD        280
